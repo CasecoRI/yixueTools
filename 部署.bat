0ab268eb966f26d567c4f51136ea8307ec9725c4
CasecoRI <102322205+CasecoRI@users.noreply.github.com>
2026-08-21 03:31:56 +0800

    .

--- 部署.bat
+++ 部署.bat
@@ -1,4 +1,4 @@
 git add .
 git commit -m "."
 cd ..
-git subtree push --prefix=.project-Relys/yixueTools https://github.com/CasecoRI/yixueTools.git
+git subtree push --prefix=.project-Relys/yixueTools https://github.com/CasecoRI/yixueTools.git master
